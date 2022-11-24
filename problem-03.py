fef12211589876ca8aae8f202d6659bb4052afd9
Dan Le Man <58541834+DanLeEpicMan@users.noreply.github.com>
2022-11-23 17:53:15 -0800

    Fixed equation error for b_k coefficient in Problem 3.

--- problem-03.py
+++ problem-03.py
@@ -1,8 +1,12 @@
 '''
 Daniel Naylor
 5094024
-11/21/2022
+11/23/2022
 '''
+
+# --------------------------------------------------------------
+#                           Question 3
+# --------------------------------------------------------------
 
 import numpy as np
 from numpy.fft import fft
@@ -32,7 +36,7 @@ b = {
     0: 0
 }
 for k in range(1, N//2 + 1):
-    b[k] = np.real((c[N-k] - c[k] - 2j)/(1j * N))
+    b[k] = np.real((c[N-k] - c[k])/(1j * N))
 
 def P_prime(x: float, /):
     '''
@@ -54,11 +58,11 @@ for j in range(8):
     x_j = j * 2 * pi / N
     print(f'Error for x_j = {j} * 2pi / N: {P_prime(x_j) - f_prime(x_j)}')
 
-# Error for x_j = 0 * 2pi / N: -1.5043179110985871
-# Error for x_j = 1 * 2pi / N: 1.5641092714881046
-# Error for x_j = 2 * 2pi / N: -1.7252592564320238e-16
-# Error for x_j = 3 * 2pi / N: -0.14989570911500927
-# Error for x_j = 4 * 2pi / N: 0.504317911098587
-# Error for x_j = 5 * 2pi / N: -0.5633896431302239
-# Error for x_j = 6 * 2pi / N: 0.9999999999999997
-# Error for x_j = 7 * 2pi / N: -0.850823919242871
+# Error for x_j = 0 * 2pi / N: -0.004317911098587035
+# Error for x_j = 1 * 2pi / N: 1.2105558808948307
+# Error for x_j = 2 * 2pi / N: -0.5000000000000002
+# Error for x_j = 3 * 2pi / N: 0.20365768147826468
+# Error for x_j = 4 * 2pi / N: 0.004317911098587146
+# Error for x_j = 5 * 2pi / N: -0.2098362525369506
+# Error for x_j = 6 * 2pi / N: 0.49999999999999994
+# Error for x_j = 7 * 2pi / N: -1.2043773098361448
